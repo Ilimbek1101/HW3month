b72ab8ef243414c1a1bf40403604f08dca13a36b
Ilimbek1101 <103297357+Ilimbek1101@users.noreply.github.com>
2022-05-17 18:09:10 +0600

    HW

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from aiogram.utils import executor
 from config import dp
-from handlers import admin, client, callback, extra, fsm_menu
+from handlers import admin, client, callback, extra, fsmAdminMenu
 import logging
 
 client.register_handler_client(dp)
 callback.register_handler_callback(dp)
 admin.register_handlers_admin(dp)
-fsm_menu.register_handler_fsmmenu(dp)
+fsmAdminMenu.register_handler_fsmmenu(dp)
 extra.register_message_handler_extra(dp)
 
 
