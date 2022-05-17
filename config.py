b3a16d40d2d38f3602844fd3cdf997552d35557b
Ilimbek1101 <103297357+Ilimbek1101@users.noreply.github.com>
2022-05-17 16:54:27 +0600

    HW3

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 from aiogram import Bot, Dispatcher
 from decouple import config
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
 
 TOKEN = config("KEYTOKEN")
 bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=storage)
 
 ADMIN = 1195853476
---
+++ handlers/fsm_menu.py
@@ -0,0 +1,76 @@
+from aiogram import types, Dispatcher
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+from config import bot
+from hwkeyboards.hw_kb import cancel_markup
+
+class FSMAdmin(StatesGroup):
+    photo = State()
+    name = State()
+    description = State()
+    price = State()
+
+async def fsm_start_menu(message: types.Message):
+    if message.chat.type == 'private':
+        await FSMAdmin.photo.set()
+        await bot.send_message(
+            message.chat.id,
+            f"Привет {message.from_user.full_name}, скинь фотку блюда",
+            reply_markup=cancel_markup
+        )
+    else:
+        await message.answer("Пиши в личку!")
+
+async def load_photo(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['photo'] = message.photo[0].file_id
+    await FSMAdmin.next()
+    await message.answer("Как называется блюдо?")
+
+async def load_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['name'] = message.text
+    await FSMAdmin.next()
+    await message.answer("Дайте описание блюда:")
+
+async def load_description(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['description'] = message.text
+    await FSMAdmin.next()
+    await message.answer("Какова цена блюда?")
+
+async def load_price(message: types.Message, state: FSMContext):
+    try:
+        async with state.proxy() as data:
+            data['price'] = int(message.text)
+        await state.finish()
+        await message.answer("Спасибо за работу!")
+    except:
+        await message.answer("Цена должна быть только в цифрах!!!")
+
+# async def load_region(message: types.Message, state: FSMContext):
+#     async with state.proxy() as data:
+#         data['region'] = message.text
+#         print(data)
+#     await state.finish()
+#     await message.answer("Все свободен!")
+
+async def cancel_registration(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    else:
+        await state.finish()
+        await message.reply("Внесение информации по блюду отменено!")
+
+def register_handler_fsmmenu(dp: Dispatcher):
+    dp.register_message_handler(cancel_registration, state='*', commands="cancel")
+    dp.register_message_handler(cancel_registration, Text(equals='cancel', ignore_case=True), state='*',)
+    dp.register_message_handler(fsm_start_menu, commands=['register'])
+    dp.register_message_handler(load_photo, state=FSMAdmin.photo, content_types=["photo"])
+    dp.register_message_handler(load_name, state=FSMAdmin.name)
+    dp.register_message_handler(load_description, state=FSMAdmin.description)
+    dp.register_message_handler(load_price, state=FSMAdmin.price)
+    # dp.register_message_handler(load_region, state=FSMAdmin.region)
