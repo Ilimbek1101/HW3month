b60142383f5e506ddb35cf639283cb36f5ca7ba8
Ilimbek1101 <103297357+Ilimbek1101@users.noreply.github.com>
2022-05-23 02:04:50 +0600

    HW5 + extratask

--- extratasks/extratask_to_hw5.py
+++ extratasks/extratask_to_hw5.py
@@ -1,7 +1,7 @@
 def make_readable(seconds):
     if type(seconds) == int and 0 <= seconds < 360000:
         HH = seconds // 3600
-        MM = (seconds - int(HH)*3600) // 60
+        MM = (seconds - HH*3600) // 60
         SS = seconds - HH*3600 - MM*60
         if HH < 10:
             HH = f"0{HH}"
@@ -13,4 +13,11 @@ def make_readable(seconds):
     else:
         print("Необходимо вводить неотрицательное целое число не более 359 999.")
 
-make_readable(180305)
+make_readable(0)
+make_readable(5)
+make_readable(60)
+make_readable(86399)
+make_readable(359999)
+make_readable(253629)
+make_readable(-3745)
+make_readable('Hello!')
